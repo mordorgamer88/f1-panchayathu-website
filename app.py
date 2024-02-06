ff607cfd92072203b7bcb3dc0c80885b87c102cf
mordorgamer88 <mordorgamer88@gmail.com>
2024-02-05 20:58:54 -0500

    hello world

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print('hello world')
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, balaj and sabraish!'
+
+if __name__ =="__main__":
+    app.run(host='0.0.0.0', debug=True)
